6e4186f13d31cb103dec6966451de2ca55e04c07
VINCENTLE <vle3@uco.edu>
2021-08-24 00:18:36 -0500

    JFrame window added

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
+import javax.swing.JFrame;
+
 public class Main{
     public static void main(String[] args)
     {
-        
+        JFrame window = new JFrame();
+        window.setSize(500, 300);
+        window.setLocation(400, 200);
+
+        window.pack();
+        window.setVisible(true);
     }
 }
